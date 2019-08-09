3ffcb091b21a6b7133c3fa5e86a5bb8f26534dbe
David Hein <david-hein196@web.de>
2019-08-09 15:27:25 +0200

    Add style sheet and title

--- modules/header.php
+++ modules/header.php
@@ -6,7 +6,8 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/bioStyle.css">
+        <title>Bio-Manager</title>
     </head>
     <body>
     <header>
