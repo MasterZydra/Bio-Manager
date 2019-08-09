3f1744fa8c2928e8663f93e4aebcb3708ccdf278
David Hein <david-hein196@web.de>
2019-08-09 16:36:27 +0200

    Change dataSetToTable() that it shows an empty table if no data was found

--- modules/helperFunctions.php
+++ modules/helperFunctions.php
@@ -1,16 +1,21 @@
 <?php
 
-function dataSetToTable($dataSet, $arrayOfColumns, $arrayOfHeadings = NULL) {
-    if ($dataSet->num_rows > 0) {
+function dataSetToTable($dataSet, $arrayOfColumns, $tableId = NULL,  $arrayOfHeadings = NULL) {
+    if (!is_null($tableId)) {
+        echo '<table id="' . $tableId . '">';
+    }
+    else {
         echo '<table>';
-        // Add headings
-        if (!is_null($arrayOfHeadings)) {
-            echo '<tr>';
-            foreach($arrayOfHeadings as $dataCol) {
-                echo '<th>' . $dataCol . '</th>';
-            }
-            echo '</tr>';
+    }
+    // Add headings
+    if (!is_null($arrayOfHeadings)) {
+        echo '<tr>';
+        foreach($arrayOfHeadings as $dataCol) {
+            echo '<th>' . $dataCol . '</th>';
         }
+        echo '</tr>';
+    }
+    if ($dataSet->num_rows > 0) {
         // Add a row in table for each data line
         while($row = $dataSet->fetch_assoc()) {
             echo '<tr>';
@@ -18,8 +23,8 @@ function dataSetToTable($dataSet, $arrayOfColumns, $arrayOfHeadings = NULL) {
                 echo '<td>' . $row[$dataCol] . '</td>';
             }
         }
-        echo '</table>';
     }
+    echo '</table>';
 }
 
 ?>
