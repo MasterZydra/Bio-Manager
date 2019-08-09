dfc5408cdfe0e5537ca1088899cc49b4f31a4dd5
David Hein <david-hein196@web.de>
2019-08-09 20:49:11 +0200

    Format text. Move div info box into echo command

--- addVendor.php
+++ addVendor.php
@@ -1,11 +1,11 @@
 <?php
     include 'modules/header_user.php';
     include 'modules/header.php';
-    ?>
+?>
 <h1>Lieferanten hinzufügen</h1>
 
 <p>
-<a href="vendors.php">Alle Lieferanten anzeigen</a>
+    <a href="vendors.php">Alle Lieferanten anzeigen</a>
 </p>
 <?php
     if(isset($_GET['add'])) {
@@ -16,35 +16,31 @@
         $conn -> dbConnect();
         
         $NULL = [
-        "type" => "null",
-        "val" => "null"
+            "type" => "null",
+            "val" => "null"
         ];
         
         $vendor_name = [
-        "type" => "char",
-        "val" => $_POST["vendor_name"]
+            "type" => "char",
+            "val" => $_POST["vendor_name"]
         ];
         
         $data = array($NULL, $vendor_name);
         
         $conn -> insertInto('T_Vendor', $data);
         $conn -> dbDisconnect();
-    ?>
-<div class="infobox">
-Der Lieferant <strong><?php echo $_POST["vendor_name"]; ?></strong> wurde hinzugefügt
-</div>
-<?php
-    
+        
+        echo '<div class="infobox">';
+        echo 'Der Lieferant <strong>' . $_POST["vendor_name"] . '</strong> wurde hinzugefügt';
+        echo '</div>';
     }
-    ?>
+?>
 <form action="?add=1" method="POST">
-<label>Name:
-<input type="text" placeholder="Name des Lieferanten" name="vendor_name" required>
-</label><br>
-<button>Hinzufügen</button>
+    <label>Name:
+        <input type="text" placeholder="Name des Lieferanten" name="vendor_name" required>
+    </label><br>
+    <button>Hinzufügen</button>
 </form>
-
 <?php
     include 'modules/footer.php';
-    ?>
-
+?>
