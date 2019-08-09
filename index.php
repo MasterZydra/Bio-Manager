9d24b4fe2a332ce6910e4773035106fc73c6c80d
David Hein <david-hein196@web.de>
2019-08-09 19:07:11 +0200

    Add header without permission check. Show different content depending on permissions.

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
 <?php
+    include 'modules/header_everyone.php';
     include 'modules/header.php';
-?>
 
-<h1>Willkommen</h1>
+    if(!isset($_SESSION['userId'])) {
+?>
+<h1>Willkommen beim Bio-Manager</h1>
+Der Bio-Manger ist zum Verwalten von Anlieferungen von Lieferanten. Durch die Eingebe der Lieferscheine, Flurstücke und Liefermenge können Rechnungen an die Kelterei und Auswertungen zu Liefermenge und der Verteilung auf Flurstücke erstellt werden.
+<?php
+}
+else {
+?>
+<h1>Willkommen im Mitgliederbereich</h1>
+
+<div>
+    <strong>Lieferanten</strong><br>
+    <a href="vendors.php">Alle Lieferanten anzeigen</a><br>
+    <a href="addVendorForm.php">Lieferanten hinzufügen</a>
+</div>
 
 <?php
+    }
     include 'modules/footer.php';
 ?>
