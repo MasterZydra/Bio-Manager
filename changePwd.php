da496b9f11e918a0f9f2ce6a423ebb60e8af662a
David Hein <david-hein196@web.de>
2019-08-09 20:51:26 +0200

    Add possibility to change password

---
+++ changePwd.php
@@ -0,0 +1,75 @@
+<?php
+    include 'modules/header_user.php';
+    include 'modules/header.php';
+
+    include 'modules/Mysql.php';
+
+    if(isset($_GET['change'])) {
+        $curPassword = $_POST['curPassword'];
+        $newPassword  = $_POST['newPassword'];
+        $newPassword2 = $_POST['newPassword2'];
+        
+        // Check old password
+        $conn = new Mysql();
+        $conn -> dbConnect();
+        $result = $conn->selectWhere('T_UserLogin', 'userId', '=', $_SESSION['userId'], 'int');
+        $conn -> dbDisconnect();
+        $conn = NULL;
+        $invalidPwd = false;
+        if($result->num_rows == 0) {
+            $invalidPwd = true;
+        }
+        else {
+            $row = $result->fetch_assoc();
+            if (!password_verify($curPassword, $row['password'])) {
+                $invalidPwd = true;
+            }
+        }
+        
+        if($invalidPwd) {
+            echo '<div class="warning">';
+            echo 'Das Passwort ist falsch. Bitte überprüfen Sie ihre Eingabe.';
+            echo '</div>';
+        }
+        elseif(strcmp($newPassword, $newPassword2) !== 0) {
+            echo '<div class="warning">';
+            echo 'Das neuen Passwörter stimmen nicht überein';
+            echo '</div>';
+        }
+        elseif(strcmp($curPassword, $newPassword) == 0) {
+            echo '<div class="warning">';
+            echo 'Das alte und neue Passwort sind identisch';
+            echo '</div>';
+        }
+        else {
+            $conn = new Mysql();
+            $conn -> dbConnect();
+            $conn -> freeRun("UPDATE T_UserLogin SET password='" . password_hash($newPassword, PASSWORD_DEFAULT) . "' WHERE userId = " . $_SESSION['userId']);
+            $conn -> dbDisconnect();
+            $conn = NULL;
+
+            echo '<div class="infobox">';
+            echo 'Das Passwort wurde geändert. Sie werden jetzt abgemeldet. <a href="logout.php">Abmelden</a> erfolgt.';
+            echo '</div>';
+            echo '<script>window.location.replace("logout.php");</script>';
+        }
+    }
+?>
+    <form action="?change=1" method="post">
+        <label>Aktuelles Passwort:<br>
+            <input type="password" size="40" maxlength="250" name="curPassword" required>
+        </label><br><br>
+
+        <label>Neues Passwort:<br>
+            <input type="password" size="40" maxlength="250" name="newPassword" required>
+        </label><br>
+
+        <label>Neues Passwort wiederholen:<br>
+            <input type="password" size="40" maxlength="250" name="newPassword2" required>
+        </label><br>
+
+        <button>Passwort ändern</button>
+    </form>
+    <?php
+    include 'modules/footer.php';
+?>
