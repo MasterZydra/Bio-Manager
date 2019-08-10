f6915acdbb04bb6199e74d28a08274fa11c9ba8a
David Hein <david-hein196@web.de>
2019-08-10 11:35:06 +0200

    Change indentation

--- deliveryNote.php
+++ deliveryNote.php
@@ -4,26 +4,26 @@
     include 'modules/header.php';
 
     include 'modules/helperFunctions.php';
-    ?>
+?>
 <script src="js/filterDataTable.js"></script>
 
 <h1>Lieferschein</h1>
 
 <!--<p>
-<a href="addDeliveryNote.php">Lieferschein hinzufügen</a>
+    <a href="addDeliveryNote.php">Lieferschein hinzufügen</a>    
 </p>-->
 <p>
-<input type="text" id="filterInput-tableDeliveryNote" onkeyup="filterData(&quot;tableDeliveryNote&quot;)" placeholder="Suchtext eingeben..." title="Suchtext">
+    <input type="text" id="filterInput-tableDeliveryNote" onkeyup="filterData(&quot;tableDeliveryNote&quot;)" placeholder="Suchtext eingeben..." title="Suchtext"> 
 </p>
 
 <?php
     $conn = new Mysql();
     $conn -> dbConnect();
     $result = $conn->selectOrderBy('T_DeliveryNote', 'year DESC, nr ASC');
-    
+
     dataSetToTable($result, array('year', 'nr', 'deliverDate', 'amount'), 'dataTable-tableDeliveryNote', array('Jahr', 'Nr', 'Lieferdatum', 'Menge'));
-    
+
     $conn->dbDisconnect();
-    
+
     include 'modules/footer.php';
-    ?>
+?>
