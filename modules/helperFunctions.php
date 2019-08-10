57dfc38b55cf78ddc07cac899b86f963381fb514
David Hein <david-hein196@web.de>
2019-08-10 11:25:44 +0200

    Add helper function to convert a data set into a table with action buttons (edit, delete).

--- modules/helperFunctions.php
+++ modules/helperFunctions.php
@@ -22,6 +22,45 @@ function dataSetToTable($dataSet, $arrayOfColumns, $tableId = NULL,  $arrayOfHea
             foreach($arrayOfColumns as $dataCol) {
                 echo '<td>' . $row[$dataCol] . '</td>';
             }
+            echo '</tr>';
+        }
+    }
+    echo '</table>';
+}
+
+function dataSetToTableWithDropdown($dataSet, $arrayOfColumns, $tableId = NULL,  $arrayOfHeadings = NULL) {
+    if (!is_null($tableId)) {
+        echo '<table id="' . $tableId . '">';
+    }
+    else {
+        echo '<table>';
+    }
+    // Add headings
+    if (!is_null($arrayOfHeadings)) {
+        echo '<tr>';
+        foreach($arrayOfHeadings as $dataCol) {
+            echo '<th>' . $dataCol . '</th>';
+        }
+        echo '</tr>';
+    }
+    if ($dataSet->num_rows > 0) {
+        // Add a row in table for each data line
+        while($row = $dataSet->fetch_assoc()) {
+            echo '<tr>';
+            foreach($arrayOfColumns as $dataCol) {
+                echo '<td>' . $row[$dataCol] . '</td>';
+            }
+            echo '<td>';
+            // Dropdown
+            echo '<div class="dropdown">';
+            echo '  <button onclick="openDropdown(' . $row['id'] . ')" class="dropbtn">Aktionen</button>';
+            echo '  <div class="dropdown-content" id="dropdown-' . $row['id'] . '">';
+            echo '    <a href="?action=edit&id=' . $row['id'] . '">Bearbeiten</a>';
+            echo '    <a href="?action=delete&id=' . $row['id'] . '">Löschen</a>';
+            echo '  </div>';
+            echo '</div>';
+            
+            echo '</td>';
         }
     }
     echo '</table>';
