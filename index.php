1ded002b052d78b3f76a6e251703919d740a0534
David Hein <david-hein196@web.de>
2019-08-11 20:53:44 +0200

    Only show links to vendors and delivery notes if user is maintainer.

--- index.php
+++ index.php
@@ -11,7 +11,10 @@ Der Bio-Manger ist zum Verwalten von Anlieferungen von Lieferanten. Durch die Ei
     } else {
 ?>
 <h1>Willkommen im Mitgliederbereich</h1>
-
+<?php
+        // Links for maintainers
+        if(isMaintainer()) {
+?>
 <div>
     <strong>Lieferant</strong><br>
     <a href="vendor.php">Alle Lieferanten anzeigen</a><br>
@@ -23,7 +26,9 @@ Der Bio-Manger ist zum Verwalten von Anlieferungen von Lieferanten. Durch die Ei
     <a href="deliveryNote.php">Alle Lieferscheine anzeigen</a><br>
     <!--<a href="addDeliveryNoter.php">Lieferanten hinzufügen</a>-->
 </div>
-
+<?php
+        }
+?>
 <div>
     <strong>Mein Konto</strong><br>
     <a href="changePwd.php">Passwort ändern</a>
