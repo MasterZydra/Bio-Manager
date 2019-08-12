10a95ab652a7126340a22488a9bb55cdc626244f
David Hein <david-hein196@web.de>
2019-08-12 21:16:46 +0200

    Add inspector to permission check

--- vendor.php
+++ vendor.php
@@ -3,7 +3,7 @@
     include 'modules/permissionCheck.php';
 
     // Check permission
-    if(!isMaintainer()) {
+    if(!isMaintainer() && !isInspector()) {
         header("Location: index.php");
         exit();
     }
