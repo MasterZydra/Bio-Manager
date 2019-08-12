6e9e8571b4dadeb96dd7b8d6a7e380e819e58028
David Hein <david-hein196@web.de>
2019-08-12 15:56:55 +0200

    Add comments

--- modules/permissionCheck.php
+++ modules/permissionCheck.php
@@ -2,29 +2,51 @@
 include 'modules/Mysql.php';
 include 'modules/Mysql_BioManager.php';
 
+/*
+* Get permission for current user.
+*/
 function checkPermission($permission) {
     return getUserPermission($_SESSION['userId'], $permission);
 }
 
+/*
+* Check if user has developer permissions
+*/
 function isDeveloper() {
     return checkPermission('isDeveloper');
 }
 
+/*
+* Check if user has adminstrator permissions
+*/
 function isAdmin() {
     return checkPermission('isAdmin');
 }
 
+/*
+* Check if user has maintainer permissions
+*/
 function isMaintainer() {
     return checkPermission('isMaintainer');
 }
 
+/*
+* Check if user has inspector permissions
+*/
 function isInspector() {
     return checkPermission('isInspector');
 }
+
+/*
+* Check if user has vendor permissions
+*/
 function isVendor() {
     return checkPermission('isVendor');
 }
 
+/*
+* Check if user is logged in
+*/
 function isLoggedIn() {
     return isset($_SESSION['userId']);
 }
