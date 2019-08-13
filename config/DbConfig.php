848eee17b79b11475cbe634044153c2f1ad39d95
David Hein <david-hein196@web.de>
2019-08-13 21:22:48 +0200

    Add documentation header

--- config/DbConfig.php
+++ config/DbConfig.php
@@ -1,5 +1,20 @@
 <?php
-// https://stackoverflow.com/questions/3228694/php-database-connection-class
+/*
+* DbConfig.php
+* ---------
+* This file contains the class 'Dbconfig'. The class contains the connection
+* data. The data is used by the Mysql class.
+* !Important! Keep your login data secret! Do not publish this file with
+* valid data!
+*
+* @Link to original soure code: https://stackoverflow.com/questions/3228694/php-database-connection-class
+*
+* @Author: David Hein
+* 
+* Changelog:
+* ----------
+*/
+
 class Dbconfig {
     protected $serverName;
     protected $userName;
