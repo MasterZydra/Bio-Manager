056691c937aaa31a79fe71c3c45ebdce8332582f
David Hein <david-hein196@web.de>
2019-08-13 16:26:58 +0200

    Renaming vendor to supplier: editVendor.php to editSupplier.php

--- editSupplier.php
+++ editSupplier.php
@@ -4,7 +4,7 @@
     
     // Check permission
     if(!isMaintainer()) {
-        header("Location: vendor.php");
+        header("Location: supplier.php");
         exit();
     }
 
@@ -14,36 +14,36 @@
 <h1>Lieferant bearbeiten</h1>
 
 <p>
-    <a href="vendor.php">Alle Lieferanten anzeigen</a>    
+    <a href="supplier.php">Alle Lieferanten anzeigen</a>    
 </p>
 
 <?php
     if(!isset($_GET['id'])) {
         echo '<div class="warning">';
-        echo 'Es wurde kein Lieferant übergeben. Zurück zu <a href="vendor.php">Alle Lieferanten anzeigen</a>';
+        echo 'Es wurde kein Lieferant übergeben. Zurück zu <a href="supplier.php">Alle Lieferanten anzeigen</a>';
         echo '</div>';
     } else {
         $conn = new Mysql();
         $conn -> dbConnect();
         
         if(isset($_GET['edit'])) {
-            $conn -> selectFreeRun(
-                'UPDATE T_Vendor '
-                . 'SET name=\'' . $_POST['vendorName'] .'\' '
-                . 'WHERE id=' . $_POST['vendorId']);
+            $conn -> freeRun(
+                'UPDATE T_Supplier '
+                . 'SET name=\'' . $_POST['supplierName'] .'\' '
+                . 'WHERE id=' . $_POST['supplierId']);
             echo '<div class="infobox">';
             echo 'Die Änderungen wurden erfolgreich gespeichert';
             echo '</div>';
         }
 
-        $result = $conn -> selectWhere('T_Vendor', 'id', '=', $_GET['id'], 'int');
+        $result = $conn -> selectWhere('T_Supplier', 'id', '=', $_GET['id'], 'int');
         $conn -> dbDisconnect();
         $conn = NULL;
         
         // Check if id is valid 
         if ($result->num_rows == 0) {
             echo '<div class="warning">';
-            echo 'Der ausgewählte Lieferant wurde in der Datenbank nicht gefunden. Zurück zu <a href="vendor.php">Alle Lieferanten anzeigen</a>';
+            echo 'Der ausgewählte Lieferant wurde in der Datenbank nicht gefunden. Zurück zu <a href="supplier.php">Alle Lieferanten anzeigen</a>';
             echo '</div>';
         } else {
             $row = $result->fetch_assoc();
@@ -52,10 +52,10 @@
 ?>
 <form action="?id=<?php echo $row['id']; ?>&edit=1" method="post">
     <label>Lieferanten-Nr:<br>
-        <input type="text" name="vendorId" value="<?php echo $row['id']; ?>" readonly>
+        <input type="text" name="supplierId" value="<?php echo $row['id']; ?>" readonly>
     </label><br>
     <label>Name:<br>
-        <input type="text" name="vendorName" value="<?php echo $row['name']; ?>">
+        <input type="text" name="supplierName" value="<?php echo $row['name']; ?>">
     </label><br>
     <button>Änderungen speichern</button>
 </form>
