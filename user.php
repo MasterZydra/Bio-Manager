ddb0d3ef55a08ebf70be935952b30cf9010fc9f8
David Hein <david-hein196@web.de>
2019-08-14 15:04:44 +0200

    Add forwarding to change user password page

--- user.php
+++ user.php
@@ -64,6 +64,10 @@
             // Action - Edit user
             // Forwording to edit page and add parameters
             echo '<script>window.location.replace("editUser.php?id=' . $_GET['id'] . '");</script>';
+        } elseif($_GET['action'] == 'changePwd') {
+            // Action - Change user password
+            // Forwording to edit page and add parameters
+            echo '<script>window.location.replace("changeUserPwd.php?id=' . $_GET['id'] . '");</script>';
         }
     }
 ?>
