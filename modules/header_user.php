0b7e8d1bca00d3e7a964cf814165ef5a194e3162
David Hein <david-hein196@web.de>
2019-08-14 14:13:00 +0200

    Add documentation header

--- modules/header_user.php
+++ modules/header_user.php
@@ -1,4 +1,15 @@
 <?php
+/*
+* header_user.php
+* ---------------
+* Include this page if only logged in users are allowed to see the page content.
+* If the visitor is not logged in, the browser forwards to index.php
+*
+* @Author: David Hein
+* 
+* Changelog:
+* ----------
+*/
     session_start();
 
     // If not logged in
