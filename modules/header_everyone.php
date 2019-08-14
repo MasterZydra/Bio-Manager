3be975a121fc61624a1baeaa53d2477fd3a9768a
David Hein <david-hein196@web.de>
2019-08-14 14:12:52 +0200

    Add documentation header

--- modules/header_everyone.php
+++ modules/header_everyone.php
@@ -1,3 +1,13 @@
 <?php
+/*
+* header_everyone.php
+* ---------------
+* Include this page if every visitor is allowed to see the page content.
+*
+* @Author: David Hein
+* 
+* Changelog:
+* ----------
+*/
     session_start();
 ?>
