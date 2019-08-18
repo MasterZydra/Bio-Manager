a240d03a3d06ef10f97540b7fb1e1b748b4b3a17
David Hein <david-hein196@web.de>
2019-08-18 23:22:03 +0200

    Add wrapper function for select element for the suppliers.

---
+++ modules/selectBox_BioManager.php
@@ -0,0 +1,35 @@
+<?php
+/*
+* selectBox_BioManager.php
+* --------------------
+* This file contains the functions to generate select elements with
+* options from a select result. It contains a wrapper functions
+* which are preconfigured for the use in the Bio-Manager project.
+*
+* @Author: David Hein
+* 
+* Changelog:
+* ----------
+*/
+
+include 'modules/selectBox.php';
+
+/**
+* Generate a select element for the suppliers.
+*
+* @param boolean    $isRequired     Flag if the field in the form is required
+*
+* @Author: David Hein
+* @return String with html code for select element
+*/
+function supplierSelectBox($isRequired) {
+    $conn = new Mysql();
+    $conn -> dbConnect();
+    $result = $conn -> select('T_Supplier', 'id AS value, name', NULL, 'name ASC');
+    $conn -> dbDisconnect();
+    $conn = NULL;
+    
+    return selectBox('supplierId', $isRequired, 'Bitte Lieferant wählen', $result);
+}
+?>
+
