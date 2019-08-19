f10e8d54471369196ee8253cd6d7a285143da8ae
David Hein <david-hein196@web.de>
2019-08-19 08:49:58 +0200

    Add logic and parameter for preselecting an option.

--- modules/selectBox.php
+++ modules/selectBox.php
@@ -9,6 +9,8 @@
 * 
 * Changelog:
 * ----------
+* 19.08.2019:
+*   - Add parameter $selectedValue
 */
 
 /**
@@ -20,11 +22,12 @@
 * @param boolean    $isRequired     Flag if the field in the form is required
 * @param string     $defaultText    This text is the preselected option. It is a placeholder.
 * @param dataSet    $nameValuePairs This dataSet contains all options that will be added
+* @param string/int $selectedValue  The option with this value will be selected. The default value is NULL
 *
 * @Author: David Hein
 * @return String with html code for select element
 */
-function selectBox($elementName, $isRequired, $defaultText, $nameValuePairs) {
+function selectBox($elementName, $isRequired, $defaultText, $nameValuePairs, $selectedValue = NULL) {
     // Starting tag
     $selectBox = '<select name="' . $elementName . '"';
     if($isRequired) {
@@ -32,12 +35,20 @@ function selectBox($elementName, $isRequired, $defaultText, $nameValuePairs) {
     }
     $selectBox .= '>';
     // Default value "select option"
-    $selectBox .= '<option value="" selected disabled>' . $defaultText . '</option>';
+    $selectBox .= '<option value="" disabled';
+    if(is_null($selectedValue)) {
+        $selectBox .= ' selected';
+    }
+    $selectBox .= '>' . $defaultText . '</option>';
     // Entries from name-value-pairs
     if($nameValuePairs -> num_rows > 0) {
         // Add a option for each row
         while($row = $nameValuePairs -> fetch_assoc()) {
-            $selectBox .= '<option value="' . $row['value'] . '">';
+            $selectBox .= '<option value="' . $row['value'] . '"';
+            if($row['value'] == $selectedValue) {
+                $selectBox .= ' selected';
+            }
+            $selectBox .= '>';
             $selectBox .= $row['name'];
             $selectBox .= '</option>';
         }
