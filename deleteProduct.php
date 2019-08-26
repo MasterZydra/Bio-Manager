086c7393ad0b893d08e3470fa82560aec7b9d991
David Hein <david-hein196@web.de>
2019-08-26 21:55:11 +0200

    add delete form for a product

---
+++ deleteProduct.php
@@ -0,0 +1,29 @@
+<?php
+/*
+* deleteProdcut.php
+* -----------------
+* This form is used to delete a product.
+*
+* @Author: David Hein
+* 
+* Changelog:
+* ----------
+*/
+    include 'templates/deleteForm.php';
+
+    $form = new deleteForm();
+    $form -> heading            = "Produkt löschen";
+
+    $form -> accessPermission   = "isMaintainer()";
+    $form -> returnPage         = "product.php";
+
+    $form -> linkPermission     = "true";
+    $form -> linkElement        = '<a href="product.php">Alle Produkte anzeigen</a>';
+    $form -> linkAllElements    = '<a href="product.php">Alle Produkte anzeigen</a>';
+
+    $form -> table              = 'T_Product';
+
+    $form -> overviewPage       = 'product.php';
+
+    $form -> show();
+?>
