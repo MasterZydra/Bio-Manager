83e6be80491386c435c6b3654c74884d4d3a4698
David Hein <david-hein196@web.de>
2019-08-26 21:45:23 +0200

    Add check if GET variable is set

--- templates/deleteForm.php
+++ templates/deleteForm.php
@@ -34,40 +34,43 @@ class deleteForm extends form {
                 \$showDialog = true;
                 if(!isset(\$_GET['id'])) {
                     echo '<div class=\"warning\">';
-                    echo 'Es wurde kein Lieferant übergeben. Zurück zu " . $this -> linkAllElements . "';
-                    echo '</div>';
-                }
-                \$conn = new Mysql();
-                \$conn -> dbConnect();
-                \$conn -> select('" . $this -> table . "', '*', 'id = ' . \$_GET['id']);
-                \$row = \$conn -> getFirstRow();
-                \$conn -> dbDisconnect();
-                \$conn = NULL;
-                // Check if id is valid
-                if (\$row == NULL) {
-                    echo '<div class=\"warning\">';
-                    echo 'Der ausgewählte Eintrag wurde in der Datenbank nicht gefunden. Zurück zu " . $this -> linkAllElements ."';
+                    echo 'Es wurde kein Eintrag übergeben. Zurück zu " . $this -> linkAllElements . "';
                     echo '</div>';
                     \$showDialog = false;
-                } else {
+                }
+                if(\$showDialog && isset(\$_GET['id'])){
                     \$conn = new Mysql();
                     \$conn -> dbConnect();
-                    if(isset(\$_GET['delete'])) {
-                        \$conn -> delete('" . $this -> table . "', 'id = ' . \$_GET['id']);
-                        echo '<div class=\"infobox\">';
-                        echo 'Der Eintrag  wurde gelöscht.';
-                        echo '</div>';
-                        \$showDialog = false;
-                    }
+                    \$conn -> select('" . $this -> table . "', '*', 'id = ' . \$_GET['id']);
+                    \$row = \$conn -> getFirstRow();
                     \$conn -> dbDisconnect();
                     \$conn = NULL;
+                    // Check if id is valid
+                    if (\$row == NULL) {
+                        echo '<div class=\"warning\">';
+                        echo 'Der ausgewählte Eintrag wurde in der Datenbank nicht gefunden. Zurück zu " . $this -> linkAllElements ."';
+                        echo '</div>';
+                        \$showDialog = false;
+                    } else {
+                        \$conn = new Mysql();
+                        \$conn -> dbConnect();
+                        if(isset(\$_GET['delete'])) {
+                            \$conn -> delete('" . $this -> table . "', 'id = ' . \$_GET['id']);
+                            echo '<div class=\"infobox\">';
+                            echo 'Der Eintrag  wurde gelöscht.';
+                            echo '</div>';
+                            \$showDialog = false;
+                        }
+                        \$conn -> dbDisconnect();
+                        \$conn = NULL;
+                    }
+                if(\$showDialog) {?>
+                    <form action=\"?id=<?php echo \$row['id']; ?>&delete=1\" method=\"post\">
+                        Wollen Sie den Eintrag wirklich löschen?<br>
+                        <button>Ja</button><button type=\"button\" onclick=\"window.location.href='" . $this -> overviewPage . "'\">Abbrechen</button>
+                    </form>
+                <?php
                 }
-            if(\$showDialog) {?>
-                <form action=\"?id=<?php echo \$row['id']; ?>&delete=1\" method=\"post\">
-                    Wollen Sie den Eintrag wirklich löschen?<br>
-                    <button>Ja</button><button type=\"button\" onclick=\"window.location.href='" . $this -> overviewPage . "'\">Abbrechen</button>
-                </form>
-            <?php
             }
             ?>";
 
