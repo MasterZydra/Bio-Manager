28df507a70262f2899665b72119c2558684277e4
David Hein <david-hein196@web.de>
2019-08-28 12:03:52 +0200

    Update database only if plot already exists. Add logic to show POST variables as field values if plot already exists.

--- editPlot.php
+++ editPlot.php
@@ -31,6 +31,7 @@
 </p>
 
 <?php
+    $alreadyExist = false;
     if(!isset($_GET['id'])) {
         echo '<div class="warning">';
         echo 'Es wurde kein Flurstück übergeben. Zurück zu <a href="plot.php">Alle Flurstücke anzeigen</a>';
@@ -39,22 +40,30 @@
         $conn = new Mysql();
         $conn -> dbConnect();
         
+        
+        $alreadyExist = isset($_POST["plot_nr"]) && alreadyExistsPlot($_POST["plot_nr"], $_GET['id']);
         if(isset($_GET['edit'])) {
-            $set = 'nr = \'' . $_POST['plot_nr'] . '\', '
-                . 'name = \'' . $_POST['plot_name'] . '\', '
-                . 'subdistrict = \'' . $_POST['plot_subdistrict'] . '\'';
-            if(!isset($_POST["supplierId"]) || !$_POST["supplierId"]) {
-                $set .= ', supplierId = NULL';
+            if($alreadyExist) {
+                echo '<div class="warning">';
+                echo 'Das Flurstück <strong>' . $_POST["plot_nr"] . '</strong> existiert bereits';
+                echo '</div>';
             } else {
-                $set .= ', supplierId = ' . $_POST["supplierId"];
+                $set = 'nr = \'' . $_POST['plot_nr'] . '\', '
+                    . 'name = \'' . $_POST['plot_name'] . '\', '
+                    . 'subdistrict = \'' . $_POST['plot_subdistrict'] . '\'';
+                if(!isset($_POST["supplierId"]) || !$_POST["supplierId"]) {
+                    $set .= ', supplierId = NULL';
+                } else {
+                    $set .= ', supplierId = ' . $_POST["supplierId"];
+                }
+                $conn -> update(
+                    'T_Plot',
+                    $set,
+                    'id = ' . $_GET['id']);
+                echo '<div class="infobox">';
+                echo 'Die Änderungen wurden erfolgreich gespeichert';
+                echo '</div>';
             }
-            $conn -> update(
-                'T_Plot',
-                $set,
-                'id = ' . $_GET['id']);
-            echo '<div class="infobox">';
-            echo 'Die Änderungen wurden erfolgreich gespeichert';
-            echo '</div>';
         }
 
         $conn -> select('T_Plot', '*', 'id = ' . $_GET['id']);
@@ -71,16 +80,43 @@
 ?>
 <form action="?id=<?php echo $row['id']; ?>&edit=1" method="post">
     <label>Nummer:<br>
-        <input type="text" name="plot_nr" value="<?php echo $row['nr']; ?>" required autofocus>
+        <input type="text" name="plot_nr" required autofocus value=
+           <?php
+                if($alreadyExist) {
+                    echo '"' . $_POST["plot_nr"] . '"';
+                } else {
+                    echo '"' . $row['nr'] . '"';
+                }
+            ?>>
     </label><br>
     <label>Name:<br>
-        <input type="text" name="plot_name" value="<?php echo $row['name']; ?>" required>
+        <input type="text" name="plot_name" required value=
+           <?php
+                if($alreadyExist) {
+                    echo '"' . $_POST["plot_name"] . '"';
+                } else {
+                    echo '"' . $row['name'] . '"';
+                }
+            ?>>
     </label><br>
     <label>Gemarkung:<br>
-        <input type="text" name="plot_subdistrict" value="<?php echo $row['subdistrict']; ?>" required>
+        <input type="text" name="plot_subdistrict" required value=
+           <?php
+                if($alreadyExist) {
+                    echo '"' . $_POST["plot_subdistrict"] . '"';
+                } else {
+                    echo '"' . $row['subdistrict'] . '"';
+                }
+            ?>>
     </label><br>
     <label>Lieferant:<br>
-        <?php echo supplierSelectBox(false, $row['supplierId'], false); ?>
+        <?php
+            if($alreadyExist && $_POST['supplierId']) {
+                echo supplierSelectBox(false, $_POST['supplierId'], false);
+            } else {
+                echo supplierSelectBox(false, $row['supplierId'], false);
+            }
+         ?>
     </label><br>
     <button>Änderungen speichern</button>
 </form>
