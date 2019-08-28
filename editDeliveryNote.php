ec6345e03d203a396d919c7fa14d07f87967b768
David Hein <david-hein196@web.de>
2019-08-28 12:33:37 +0200

    Set year and number of delivery note to readonly.

--- editDeliveryNote.php
+++ editDeliveryNote.php
@@ -41,9 +41,7 @@
         
         if(isset($_GET['edit'])) {
             // Build set part of query
-            $set = 'year = ' . $_POST['note_year'] . ', '
-                . 'nr = ' . $_POST['note_number'] . ', '
-                . 'productId = ' . $_POST['productId'];
+            $set = 'productId = ' . $_POST['productId'];
             // Deliver date
             if($_POST['note_date']) {
                 $set .= ', deliverDate = \'' . $_POST['note_date'] . '\'';
@@ -85,22 +83,22 @@
 ?>
 <form action="?id=<?php echo $row['id']; ?>&edit=1" method="post">
     <label>Jahr:<br>
-        <input type="number" name="note_year" value="<?php echo $row['year']; ?>" required autofocus>
+        <input type="number" value="<?php echo $row['year']; ?>" readonly>
     </label><br>
     <label>Nummer:<br>
-        <input type="number" name="note_number" value="<?php echo $row['nr']; ?>" required>
-    </label><br>
-    <label>Produkt:<br>
-        <?php echo productSelectBox(NULL, $row['productId']); ?>
+        <input type="number" value="<?php echo $row['nr']; ?>" readonly>
     </label><br>
     <label>Lieferdatum:<br>
-        <input type="date" name="note_date" value="<?php echo $row['deliverDate']; ?>">
+        <input type="date" name="note_date" value="<?php echo $row['deliverDate']; ?>" autofocus>
     </label><br>
     <label>Liefermenge:<br>
         <input type="number" name="note_amount" value="<?php echo $row['amount']; ?>" placeholder="Menge eingeben">
     </label><br>
     <label>Lieferant:<br>
         <?php echo supplierSelectBox(false, $row['supplierId'], false); ?>
+    </label><br>
+    <label>Produkt:<br>
+        <?php echo productSelectBox(NULL, $row['productId']); ?>
     </label><br>
     <button>Änderungen speichern</button>
 </form>
