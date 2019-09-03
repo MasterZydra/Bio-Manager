73e67c3a36d166f71767274b37a2305349137e04
David Hein <david-hein196@web.de>
2019-09-03 15:15:12 +0200

    Add documentation for class deleteForm

--- templates/deleteForm.php
+++ templates/deleteForm.php
@@ -12,16 +12,50 @@
 
 include 'templates/form.php';
 
+/**
+* The class form is generating an HTML template for a delete page.
+* The content is set through properties. Indiviudal code can be added too.
+*
+* With heredity the class can be expanded for specialized pages.
+*
+* @author David Hein
+*/
 class deleteForm extends form {
+    /**
+    * HTML code which will be shown in message box as link to all elements.
+    * E.g. '<a href="pricing.php">Alle Preise anzeigen</a>'
+    * @var string
+    */
     public $linkAllElements;
-    
+
+    /**
+    * Table name where entry with given id will be deleted. E.g. 'T_User'
+    * @var string
+    */
     public $table;
     
+    /**
+    * Page which will be forwarded to after deleting. E.g. 'user.php'
+    * @var string
+    */
     public $overviewPage;
     
+    /**
+    * Code that will be executed before the call of the delete query.
+    * With $conn the database connection can be called.
+    * With $_GET['id'] the given id can be accessed.
+    * E.g. "\$conn -> delete('T_UserLogin', 'userId=' . \$_GET['id']);"
+    * @var string
+    */
     public $queryBeforeDelete;
     
+    /**
+    * Construct a new deleteForm object and set default values for the properties
+    *
+    * @author David Hein
+    */
     function __construct() {
+        // Create parent
         parent::__construct();
         $this -> linkAllElements    = '<a href="index.php">Startseite</a>';
         
@@ -32,6 +66,13 @@ class deleteForm extends form {
         $this -> queryBeforeDelete  = '';
     }
     
+    /**
+    * Show the page content
+    *
+    * @param string $ownCode    If this parameter is set, the given string will be excuted
+    *
+    * @author David Hein
+    */
     function show($ownCode = "") {
         $code = "
             <?php
