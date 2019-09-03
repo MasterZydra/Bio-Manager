c1855c30c626e2db1cdb3f6e8e84f813dde39641
David Hein <david-hein196@web.de>
2019-09-03 14:34:59 +0200

    Add form for deleting users.

---
+++ deleteUser.php
@@ -0,0 +1,32 @@
+<?php
+/*
+* deleteUser.php
+* --------------
+* This form is used to delete an user.
+*
+* @Author: David Hein
+* 
+* Changelog:
+* ----------
+*/
+    include 'templates/deleteForm.php';
+
+    $form = new deleteForm();
+    $form -> heading            = "Benutzer löschen";
+
+    $form -> accessPermission   = "isAdmin()";
+    $form -> returnPage         = "user.php";
+
+    $form -> linkPermission     = "true";
+    $form -> linkElement        = '<a href="user.php">Alle Benutzer anzeigen</a>';
+    $form -> linkAllElements    = '<a href="user.php">Alle Benutzer anzeigen</a>';
+
+    $form -> table              = 'T_User';
+
+    $form -> overviewPage       = 'user.php';
+
+    $form -> queryBeforeDelete  = "\$conn -> delete('T_UserLogin', 'userId=' . \$_GET['id']);" .
+                                  "\$conn -> delete('T_UserPermission', 'userId=' . \$_GET['id']);";
+
+    $form -> show();
+?>
