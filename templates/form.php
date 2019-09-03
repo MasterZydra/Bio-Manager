35d37915090cfec1e0986150a9f073db6fd59c19
David Hein <david-hein196@web.de>
2019-09-03 14:42:22 +0200

    Add documentation for form class

--- templates/form.php
+++ templates/form.php
@@ -20,17 +20,54 @@ function betterEval($code) {
     return $ret;
 }
 
+/**
+* The class form is generating an HTML template. The content
+* is set through properties. Indiviudal code can be added too.
+*
+* With heredity the class can be expanded for specialized pages.
+*
+* @author David Hein
+*/
 class form {
+    /**
+    * Permission that is necessary to access this page. The string is inserted as if condition. E.g. "isMaintainer()"
+    * @var string
+    */
     public $accessPermission;
+    /**
+    * This page will be called if the user has not enough permission to access the page. E.g. "user.php"
+    * @var string
+    */
     public $returnPage;
     
+    /**
+    * Permission that is necessary to view the elements in $linkElement. The string is inserted as if condition. E.g. "true" or "isMaintainer()"
+    * @var string
+    */
     public $linkPermission;
+    /**
+    * HTML code which will be shown if $linkPermission is true. E.g. '<a href="pricing.php">Alle Preise anzeigen</a>'
+    * @var string
+    */
     public $linkElement;
     
+    /**
+    * H1 heading which is shown on the page content.
+    * @var string
+    */
     public $heading;
     
+    /**
+    * Flag if caching shall be activated for the page. The default value is "false". To activate caching set the value to "true".
+    * @var string
+    */
     public $caching;
     
+    /**
+    * Construct a new form class and set default values for the properties
+    *
+    * @author David Hein
+    */
     function __construct() {
         $this -> accessPermission   = false;
         $this -> returnPage         = "index.php";
@@ -43,6 +80,13 @@ class form {
         $this -> caching            = "false";
     }
     
+    /**
+    * Show the page content
+    *
+    * @param string $ownCode    If this parameter is set, the given string will be excuted
+    *
+    * @author David Hein
+    */
     function show($ownCode = "") {        
         betterEval(
             file_get_contents('modules/header_user.php') .
