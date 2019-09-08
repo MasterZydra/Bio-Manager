2df04bfb0e02e984911bcf72c28a53df42454f18
David Hein <david-hein196@web.de>
2019-09-08 20:53:19 +0200

    Add form to delete a recipient

---
+++ deleteRecipient.php
@@ -0,0 +1,29 @@
+<?php
+/*
+* deleteRecipient.php
+* -------------------
+* This form is used to delete a recipient.
+*
+* @Author: David Hein
+* 
+* Changelog:
+* ----------
+*/
+    include 'templates/deleteForm.php';
+
+    $form = new deleteForm();
+    $form -> heading            = "Abnehmer löschen";
+
+    $form -> accessPermission   = "isMaintainer()";
+    $form -> returnPage         = "recipient.php";
+
+    $form -> linkPermission     = "true";
+    $form -> linkElement        = '<a href="recipient.php">Alle Abnehmer anzeigen</a>';
+    $form -> linkAllElements    = '<a href="recipient.php">Alle Abnehmer anzeigen</a>';
+
+    $form -> table              = 'T_Recipient';
+
+    $form -> overviewPage       = 'recipient.php';
+
+    $form -> show();
+?>
