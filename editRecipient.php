0701d17104f11256411ce56f813515d330a0a86b
David Hein <david-hein196@web.de>
2019-09-08 21:12:29 +0200

    Add form to edit a recipient

---
+++ editRecipient.php
@@ -0,0 +1,89 @@
+<?php
+/*
+* editRecipient.php
+* -----------------
+* This form is used to edit a recipient.
+*
+* @Author: David Hein
+* 
+* Changelog:
+* ----------
+*/
+    include 'modules/header_user.php';
+    include 'modules/permissionCheck.php';
+    
+    // Check permission
+    if(!isMaintainer()) {
+        header("Location: recipient.php");
+        exit();
+    }
+
+    include 'modules/header.php';
+?>
+
+<h1>Abnehmer bearbeiten</h1>
+
+<p>
+    <a href="recipient.php">Alle Abnehmer anzeigen</a>    
+</p>
+
+<?php
+    $alreadyExist = false;
+    if(!isset($_GET['id'])) {
+        echo '<div class="warning">';
+        echo 'Es wurde kein Abnehmer übergeben. Zurück zu <a href="recipient.php">Alle Abnehmer anzeigen</a>';
+        echo '</div>';
+    } else {
+        $conn = new Mysql();
+        $conn -> dbConnect();
+        
+        $alreadyExist = isset($_POST["recipient_name"]) && alreadyExistsRecipient($_POST["recipient_name"], $_GET['id']);
+        if(isset($_GET['edit'])) {
+            if($alreadyExist) {
+                echo '<div class="warning">';
+                echo 'Der Abnehmer <strong>' . $_POST["recipient_name"] . '</strong> existiert bereits';
+                echo '</div>';
+            } else {
+                $conn -> update(
+                    'T_Recipient',
+                    'name = \'' . $_POST['recipient_name'] . '\', '
+                    . 'address = \'' . $_POST['recipient_address'] . '\'',
+                    'id = ' . $_GET['id']);
+                echo '<div class="infobox">';
+                echo 'Die Änderungen wurden erfolgreich gespeichert';
+                echo '</div>';
+            }
+        }
+
+        $conn -> select('T_Recipient', '*', 'id = ' . $_GET['id']);
+        $row = $conn -> getFirstRow();
+        $conn -> dbDisconnect();
+        $conn = NULL;
+        
+        // Check if id is valid 
+        if ($row == NULL) {
+            echo '<div class="warning">';
+            echo 'Der ausgewählte Abnehmer wurde in der Datenbank nicht gefunden. Zurück zu <a href="recipient.php">Alle Abnehmer anzeigen</a>';
+            echo '</div>';
+        }
+    }
+?>
+<form action="?id=<?php echo $row['id']; ?>&edit=1" method="post">
+    <label>Name:<br>
+        <input type="text" name="recipient_name" placeholder="Name des Abnehmers" required autofocus value=
+            <?php
+                if($alreadyExist) {
+                    echo '"' . $_POST["recipient_name"] . '"';
+                } else {
+                    echo '"' . $row['name'] . '"';
+                }
+            ?>>
+    </label><br>
+    <label>Anschrift:<br>
+        <textarea name="recipient_address" placeholder="Adresse des Abnehmers" required><?php if($alreadyExist) { echo $_POST["recipient_address"]; } else { echo $row['address']; } ?></textarea>
+    </label><br>
+    <button>Änderungen speichern</button>
+</form>
+<?php
+    include 'modules/footer.php';
+?>
