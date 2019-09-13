bfc0a61775302cd1ae8fd7b3bfb772724321fbdc
David Hein <david-hein196@web.de>
2019-09-13 12:08:43 +0200

    Add required class for star for required fields

--- editDeliveryNote.php
+++ editDeliveryNote.php
@@ -88,9 +88,10 @@
     <label>Nummer:<br>
         <input type="number" value="<?php echo $row['nr']; ?>" readonly>
     </label><br>
-    <label>Lieferdatum:<br>
-        <input type="date" name="note_date" value="<?php echo $row['deliverDate']; ?>" autofocus>
-    </label><br>
+    
+    <label for="note_date" class="required">Lieferdatum:</label><br>
+    <input id="note_date" name="note_date" type="date" value="<?php echo $row['deliverDate']; ?>" autofocus><br>
+    
     <label>Liefermenge (in <?php echo getSetting('volumeUnit'); ?>):<br>
         <input type="number" name="note_amount" value="<?php echo $row['amount']; ?>" placeholder="Menge eingeben">
     </label><br>
