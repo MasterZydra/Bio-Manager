5c046ed1ce69602279c750df84699f0976a2df97
David Hein <david-hein196@web.de>
2019-09-13 12:10:02 +0200

    Remove year from copy right

--- modules/footer.php
+++ modules/footer.php
@@ -2,7 +2,7 @@
     <footer>
         <a href="impressum.php">Impressum</a>&nbsp;&nbsp;&nbsp;&nbsp;
         <?php if(isLoggedIn()) { ?><a href="about.php">Systeminformationen</a>&nbsp;&nbsp;&nbsp;&nbsp;<?php } ?>
-        David Hein &copy; 2019
+        &copy; David Hein
     </footer>
     </body>
 </html>
