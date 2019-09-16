c723be4ee9a3951bfd3620e16bd8d2255ccdc829
David Hein <david-hein196@web.de>
2019-09-16 18:46:27 +0200

    Rebuild form class so the eval function in not necessary anymore. Split logic in show() which calls the functions, head() for start part and foot() for closing part. Add config for other variables.

--- templates/form.php
+++ templates/form.php
@@ -11,25 +11,6 @@
 */
 
 /**
-* Executing given PHP code
-*
-* Source of function: https://www.php.net/manual/en/function.eval.php
-*
-* @param $code  PHP code which shall be executed
-*
-* @return HTML code
-*/
-function betterEval($code) {
-    $tmp = tmpfile ();
-    $tmpf = stream_get_meta_data ( $tmp );
-    $tmpf = $tmpf ['uri'];
-    fwrite ( $tmp, $code );
-    $ret = include ($tmpf);
-    fclose ( $tmp );
-    return $ret;
-}
-
-/**
 * The class form is generating an HTML template. The content
 * is set through properties. Indiviudal code can be added too.
 *
@@ -39,23 +20,29 @@ function betterEval($code) {
 */
 class form {
     /**
-    * Permission that is necessary to access this page. The string is inserted as if condition. E.g. "isMaintainer()"
-    * @var string
+    * Permission that is necessary to access this page.
+    * If it is a string e.g. "isMaintainer", the function will be executed.
+    * If it is a boolean, it will directly used as permission check.
+    * @var string/boolean
     */
     public $accessPermission;
     /**
-    * This page will be called if the user has not enough permission to access the page. E.g. "user.php"
+    * This page will be called if the user has not enough permission
+    * to access the page. E.g. "user.php"
     * @var string
     */
     public $returnPage;
     
     /**
-    * Permission that is necessary to view the elements in $linkElement. The string is inserted as if condition. E.g. "true" or "isMaintainer()"
-    * @var string
+    * Permission that is necessary to view the elements in $linkElement.
+    * If it is a string e.g. "isMaintainer", the function will be executed.
+    * If it is a boolean, it will directly used as permission check.
+    * @var string/boolean
     */
     public $linkPermission;
     /**
-    * HTML code which will be shown if $linkPermission is true. E.g. '<a href="pricing.php">Alle Preise anzeigen</a>'
+    * HTML code which will be shown if $linkPermission is true.
+    * E.g. '<a href="pricing.php">Alle Preise anzeigen</a>'
     * @var string
     */
     public $linkElement;
@@ -67,10 +54,17 @@ class form {
     public $heading;
     
     /**
-    * Flag if caching shall be activated for the page. The default value is "false". To activate caching set the value to "true".
-    * @var string
+    * Flag if caching shall be activated for the page.
+    * The default value is "false". To activate caching set the value to "true".
+    * @var boolean
     */
     public $caching;
+    
+    /**
+    * Config for other contents. It is e.g. used for transfer the cache file name.
+    * @var array
+    */
+    protected $config;
     
     /**
     * Construct a new form object and set default values for the properties
@@ -86,37 +80,79 @@ class form {
         
         $this -> heading            = "Überschrift";
         
-        $this -> caching            = "false";
+        $this -> caching            = false;
+        
+        $this -> config["cacheFile"] = "";
     }
-    
+
     /**
-    * Show the page content
-    *
-    * @param string $ownCode    If this parameter is set, the given string will be excuted
+    * Head part of a page. It contains permission check, caching and heading.
     *
     * @author David Hein
     */
-    function show($ownCode = "") {        
-        betterEval(
-            file_get_contents('modules/header_user.php') .
-            file_get_contents('modules/permissionCheck.php').
-            // Check permission
-            "<?php
-            if(!" . $this -> accessPermission . ") {
-                header(\"Location: " . $this -> returnPage . "\");
-                exit();
-            } ?>" .
-            // Caching
-            "<?php if(" . $this -> caching . ") { ?>" . file_get_contents('modules/cache_top.php') . " <?php } ?>" .
-            file_get_contents('modules/header.php') .
-            "<h1>" . $this -> heading . "</h1>" .
-            "<p>" .
-            "<?php if(" . $this -> linkPermission . ") {?>" . $this -> linkElement . "<?php } ?>" .
-            "</p>" .
-            // Code of child class
-            (string)$ownCode .
-            // Caching
-            "<?php if(" . $this -> caching . ") {?>" . file_get_contents('modules/cache_bottom.php') . "<?php } ?>");
+    protected function head() {
+        include 'modules/header_user.php';
+        include 'modules/permissionCheck.php';
+
+        // Execute access permission function
+        if (is_string($this -> accessPermission)) {
+            $permissionFunc = $this -> accessPermission;
+            $this -> accessPermission = $permissionFunc();
+        }
+        
+        // Execute link permission function
+        if (is_string($this -> caching)) {
+            $permissionFunc = $this -> caching;
+            $this -> caching = $permissionFunc();
+        }
+        
+        // Check permission
+        if(!$this -> accessPermission) {
+            header("Location: " . $this -> returnPage);
+            exit();
+        }
+
+        // Caching        
+        if($this -> caching) {
+            include 'modules/cache.php';
+            $this -> config["cacheFile"] = $config["cacheFile"];
+        }
+        
+        include 'modules/header.php';
+        
+        // Heading and link
+        echo "<h1>" . $this -> heading . "</h1>";
+        echo "<p>";
+        if($this -> linkPermission) {
+            echo $this -> linkElement;
+        }
+        echo "</p>";
+    }
+
+    /**
+    * Foot part of a page. It contains the closing the footer
+    * and part of caching.
+    *
+    * @author David Hein
+    */
+    protected function foot() {
+        include 'modules/footer.php';
+        
+        // Caching
+        if($this -> caching) {
+            writeCacheFile($this -> config["cacheFile"]);
+        }
+    }
+    
+    /**
+    * Main function which calls all parts that will be executed.
+    * It creates the page which will be shown.
+    *
+    * @author David Hein
+    */
+    public function show() {
+        $this -> head();
+        $this -> foot();
     }
 }
 
