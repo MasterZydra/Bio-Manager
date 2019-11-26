5d7a73f7a8cd0bf578b632e5408ec0fd9603cac5
David Hein <david-hein196@web.de>
2019-11-26 16:54:22 +0100

    Add config for invoice data instead of saving them in settings DB

---
+++ config/InvoiceDataConfig.php
@@ -0,0 +1,13 @@
+<?php
+    // Configuration for data which will be shown in invoice
+    // Important: Use web interface to change the data. This file will be written from there.
+    $invoice["sender_name"] = "Max Mustermann";
+    $invoice["sender_address"] = "Musterstraße 1";
+    $invoice["sender_postalCode"] = "12345";
+    $invoice["sender_city"] = "Musterstadt";
+    $invoice["bank"] = "Musterbank Muster";
+    $invoice["BIC"] = "GEXXXXXXXXX";
+    $invoice["IBAN"] = "DEXXXXXXXXX00000XXXXXX";
+    $invoice["author"] = "Max Mustermann";
+    $invoice["name"] = "Rechnung";
+?>
