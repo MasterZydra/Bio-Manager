2bc8a4b80b30436146cb269845d4793135607d69
David Hein <david-hein196@web.de>
2019-12-30 21:29:33 +0100

    Change login to dummy data

--- config/DatabaseConfig.php
+++ config/DatabaseConfig.php
@@ -2,7 +2,7 @@
     // Configuration for database connection
     // Important: Use web interface to change the data. This file will be written from there.
     $database["server_name"] = "localhost";
-    $database["database_name"] = "bioMan_Dev_HSW";
-    $database["database_username"] = "bioHeinAdmin";
-    $database["database_password"] = "jajgy2-towsyw-qoZryq";
+    $database["database_name"] = "bioManagerDB";
+    $database["database_username"] = "username";
+    $database["database_password"] = "password123";
 ?>
