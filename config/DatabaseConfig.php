9fc40c82d12544514c2c2b6a28ec679f88d4e82e
David Hein <david-hein196@web.de>
2020-01-20 11:33:40 +0100

    Remove configs

--- config/DatabaseConfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-    // Configuration for database connection
-    // Important: Use web interface to change the data. This file will be written from there.
-    $database["server_name"] = "localhost";
-    $database["database_name"] = "bioManagerDB";
-    $database["database_username"] = "username";
-    $database["database_password"] = "password123";
-?>
