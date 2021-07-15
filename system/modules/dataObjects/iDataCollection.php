baeb0ff245f0e056b90b9320c7fdbe0a2f9bd9bc
David Hein <david-hein196@web.de>
2021-07-15 19:21:28 +0200

    Add and implement the add function for supplier

--- system/modules/dataObjects/iDataCollection.php
+++ system/modules/dataObjects/iDataCollection.php
@@ -15,6 +15,7 @@ interface iDataCollection
     public function find(int $id) : iObject;
     public function findAll();
     public function delete(int $id);
+    public function add(iObject $object) : bool;
     public function update(iObject $object) : bool;
 }
 
