060e2da5eb93c0741c82aa2bb3bbf3e8d7fa746c
David Hein <david-hein196@web.de>
2021-07-15 19:49:55 +0200

    Generalize supplierAlreadyExists to objectAlreadyExists so it can be used for all iObject implementations

--- addSupplier.php
+++ addSupplier.php
@@ -26,14 +26,14 @@
     <a href="supplier.php">Alle Lieferanten anzeigen</a>
 </p>
 <?php
-    $alreadyExist = isset($_POST["supplier_name"]) && alreadyExistsSupplier(secPOST("supplier_name"));
+    $supplierColl = new SupplierCollection();
+    $alreadyExist = isset($_POST["supplier_name"]) && MySQL_helpers::objectAlreadyExists($supplierColl, secPOST("supplier_name"), 0);
     if(isset($_GET['add'])) {
         if($alreadyExist) {
             echo '<div class="warning">';
             echo 'Der Lieferant <strong>' . secPOST("supplier_name") . '</strong> existiert bereits';
             echo '</div>';
         } else {
-            $supplierColl = new SupplierCollection();
             $newSupplier = new Supplier(0, secPOST("supplier_name"), false);
             if($supplierColl->add($newSupplier)) {
                 echo '<div class="infobox">';
