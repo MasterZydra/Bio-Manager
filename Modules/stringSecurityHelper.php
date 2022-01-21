65b20066f00ff15dd91ebe38f53ca406eb2a5a84
MasterZydra <david-hein196@web.de>
2022-01-21 20:15:04 +0100

    Fix getPost and secGet to work with PHP8

--- Modules/stringSecurityHelper.php
+++ Modules/stringSecurityHelper.php
@@ -5,12 +5,26 @@ function getSecuredString(string $string)
     return htmlspecialchars(trim($string));
 }
 
-function secPOST($name)
+function secPOST(string $name)
 {
-    return getSecuredString($_POST[$name]);
+    if (!isset($_POST[$name])) {
+        return null;
+    }
+    $value = $_POST[$name];
+    if (!is_string($value)) {
+        return $value;
+    }
+    return getSecuredString($value);
 }
 
-function secGET($name)
+function secGET(string $name)
 {
-    return getSecuredString($_GET[$name]);
+    if (!isset($_GET[$name])) {
+        return null;
+    }
+    $value = $_GET[$name];
+    if (!is_string($value)) {
+        return $value;
+    }
+    return getSecuredString($value);
 }
