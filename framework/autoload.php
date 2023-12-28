da2bf8bcbb0096c6871db34a6a44480070254aa4
MasterZydra <david-hein196@web.de>
2023-12-28 22:59:46 +0100

    Readd improved autoloader

---
+++ framework/autoload.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Framework;
+
+spl_autoload_register(
+    function ($class) {
+        $prefixes = [
+            'Framework\\' => '',
+            'App\\' => 'app\\',
+        ];
+
+        foreach ($prefixes as $prefix => $dir) {
+            if (!str_starts_with($class, $prefix)) {
+                continue;
+            }
+
+            $filename = str_replace($prefix, $dir, $class);
+            if ($dir === '') {
+                $filename = __DIR__ . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $filename) . '.php';
+            } else {
+                $filename = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $filename) . '.php';
+            }
+
+            if (!file_exists($filename)) {
+                throw new \Exception('Requested file "' . $class . '" not found');
+            }
+            
+            require_once $filename;
+            return;
+        }
+    }
+);
