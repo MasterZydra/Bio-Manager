51ee85629879f0bbaf7acab0c1e2ae764f08d4b9
MasterZydra <david-hein196@web.de>
2024-01-23 20:28:57 +0100

    Set error reporting and display_errors to enabled only in dev mode

--- framework/Error/RegisterErrorHandler.php
+++ framework/Error/RegisterErrorHandler.php
@@ -1,7 +1,13 @@
 <?php
 
+use Framework\Config\Config;
 use Framework\Error\ErrorHandler;
 
 set_error_handler(fn(int $errno, string $errstr, ?string $errfile = null, ?int $errline = null, ?array $errcontext = null) => ErrorHandler::handleError($errno, $errstr, $errfile, $errline, $errcontext));
 
 set_exception_handler(fn(Throwable $exception) => ErrorHandler::handleException($exception));
+
+if (strtolower(Config::env('APP_ENV')) === 'dev') {
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+}
