7d1a1f563611e2238b09604c169dd9924f10c3f7
MasterZydra <david-hein196@web.de>
2024-02-18 19:11:49 +0100

    Added seeder for invoice settings

---
+++ framework/Database/Seeders/InvoiceSettingsSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Framework\Database\Seeders;
+
+use App\Models\Setting;
+use Framework\Database\Database;
+use Framework\Database\Seeder;
+use Framework\Database\SeederInterface;
+
+class InvoiceSettingsSeeder extends Seeder implements SeederInterface
+{
+    public function run(): void
+    {
+        // Invoice
+        (new Setting())
+            ->setName('invoiceSender')
+            ->setDescription('Invoice sender')
+            ->setValue('Invoice sender name')
+            ->save();
+        
+        (new Setting())
+            ->setName('invoiceBankName')
+            ->setDescription('Invoice bank name')
+            ->setValue('Invoice bank name')
+            ->save();
+
+        (new Setting())
+            ->setName('invoiceIBAN')
+            ->setDescription('Invoice IBAN')
+            ->setValue('DEXX XXXX ...')
+            ->save();
+        
+        (new Setting())
+            ->setName('invoiceBIC')
+            ->setDescription('Invoice BIC')
+            ->setValue('GEXXXX...')
+            ->save();
+
+        (new Setting())
+            ->setName('invoiceAuthor')
+            ->setDescription('Invoice author name')
+            ->setValue('Invoice author name')
+            ->save();
+
+        (new Setting())
+            ->setName('invoiceName')
+            ->setDescription('Invoice name')
+            ->setValue('Rechnung')
+            ->save();
+    }
+}
